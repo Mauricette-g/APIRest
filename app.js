794fa9cf812f93881cc2b153fa1108da5049e398
Mauricette-g <mauricettejoel17@gmail.com>
2025-07-09 15:34:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ const indexRouter = require('./routes/index');
 
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, './frontend')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+});
 
 
 // Routes
