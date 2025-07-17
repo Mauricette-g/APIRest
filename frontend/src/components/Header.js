6a774d1ffe1df545d17c39b32baabc3a025265eb
Mauricette-g <mauricettejoel17@gmail.com>
2025-07-18 01:25:31 +0200

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Header() {
 
@@ -8,15 +8,15 @@ function Header() {
   return (
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom"> 
         <div class="col-md-3 mb-2 mb-md-0"> 
-            <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none"> 
+            <Link to="/"  class="d-inline-flex link-body-emphasis text-decoration-none"> 
             <h1 class="px-2">Port de Russel</h1>
-            </a> 
+            </Link> 
         </div> 
  
         <div class="nav col-12 col-md-auto mb-2 px-2"> 
-            <li><a href="/" class="nav-link px-2 link-secondary">Accueil</a></li> 
-            <button type="button" class="btn btn-outline-primary me-2"><a href="/login" class="nav-link px-2 link-secondary">Connexion </a></button> 
-            <button type="button" class="btn btn-primary"><a href="/register" class="nav-link px-2 link-secondary">S'inscrire</a></button> 
+            <li><Link to="/"  class="nav-link px-2 link-secondary">Accueil</Link></li> 
+            <button type="button" class="btn btn-outline-primary me-2"><Link to="/login" class="nav-link px-2 link-secondary">Connexion </Link></button> 
+            <button type="button" class="btn btn-primary"><Link to="/" ="/register" class="nav-link px-2 link-secondary">S'inscrire</Link></button> 
         </div> 
     </header> 
 
